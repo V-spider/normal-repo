534b5cb878e33a9e96ecc946ab0d877c37eb6028
V.spider <mahmudhassen729@gmail.com>
2023-04-12 16:46:27 +0300

    fixes #2

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,8 @@
 var search = document.querySelector(".header-search");
 var modal = document.querySelector(".modal-diffuser");
+var ham = document.querySelector(".hum");
+var mobileNav = document.querySelector(".mobile-nav");
+
 search.addEventListener('click', (e) => {
     console.log(modal.classList)
     modal.classList.remove('hidden');
@@ -11,3 +14,23 @@ modal.addEventListener("click", (e) => {
   modal.classList.add("hidden");
   
 });
+
+
+
+ham.addEventListener('click', function (e) {
+  if (!(ham.getAttribute("data-open") == "open")) {
+    // close it
+    ham.setAttribute("data-open", "open");
+    for (let index = 0; index < ham.children.length; index++) {
+      ham.children[index].classList.remove(`rotate-line${index + 1}`);
+    }
+    mobileNav.style.display = "none";
+  } else {
+    // open it\
+    ham.setAttribute("data-open", "close");
+    for (let index = 0; index < ham.children.length; index++) {
+      ham.children[index].classList.add(`rotate-line${index + 1}`);
+    }
+    mobileNav.style.display = 'block';
+  }
+})
